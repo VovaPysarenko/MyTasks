9b326960f54e93e676a986e983cdc1c3eec04e84
Vladislav <vnikolaychuk@atncorp.com>
2021-08-19 13:31:02 +0300

    main: added fix for work with BD.

--- MyTasks/Controlers/AddTaskTableViewController.swift
+++ MyTasks/Controlers/AddTaskTableViewController.swift
@@ -11,23 +11,27 @@ import Firebase
 class AddTaskTableViewController: UITableViewController {
     
     var postData = [String]()
-    var ref: DatabaseReference?
     var databaseHandle: DatabaseHandle?
+    private let refDatabase = Database.database().reference().child("Posts")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        ref = Database.database().reference()
-        
-        databaseHandle = ref?.child("Posts").observe(.childAdded, with: {[weak self] (snapshot) in
-            
-            let post = snapshot.value as? String
-            if let actualPost = post {
-                self?.postData.append(actualPost)
-                
-                self?.tableView.reloadData()
-            }
+        self.refDatabase.observeSingleEvent(of: .value, with: { [weak self] snapshot in
+            guard let self = self else {return}
+            print("getAllPosts: \(snapshot) ")
         })
+        
+//        ref = Database.database().reference()
+//
+//        databaseHandle = ref?.child("Posts").observe(.childAdded, with: {[weak self] (snapshot) in
+//
+//            let post = snapshot.value as? String
+//            if let actualPost = post {
+//                self?.postData.append(actualPost)
+//
+//                self?.tableView.reloadData()
+//            }
+//        })
 
     }
 
@@ -38,7 +42,7 @@ class AddTaskTableViewController: UITableViewController {
 //        return 0
 //    }
     @IBAction func addPressed(_ sender: UIBarButtonItem) {
-        ref?.child("Posts").childByAutoId().setValue("Hello")
+        refDatabase.childByAutoId().child("name").setValue("Hello")
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
